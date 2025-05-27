19958641eb790470036ae0fee38fec68379b4791
barkmetax <hi@barkmeta.io>
2025-05-27 16:52:16 -0400

    Update btc-balance.ts

--- pages/api/btc-balance.ts
+++ pages/api/btc-balance.ts
@@ -1,11 +1,11 @@
-
 export default async function handler(req, res) {
   try {
-    const response = await fetch('https://blockchain.info/q/addressbalance/39mS67GHiLbZZYR4RhewiEhBVc65hUjskc');
-    const sats = await response.text();
-    const btc = parseFloat(sats) / 1e8;
+    const wallet = '39mS67GHiLbZZYR4RhewiEhBVc65hUjskc';
+    const response = await fetch(`https://blockstream.info/api/address/${wallet}`);
+    const data = await response.json();
+    const btc = (data.chain_stats.funded_txo_sum - data.chain_stats.spent_txo_sum) / 1e8;
     res.status(200).json({ btc });
-  } catch (error) {
+  } catch (err) {
     res.status(500).json({ error: 'BTC fetch failed' });
   }
 }
